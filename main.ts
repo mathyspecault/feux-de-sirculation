b3abca4115d868f39e7234ec9235840ea6829183
mathyspecault <117740980+mathyspecault@users.noreply.github.com>
2022-11-29 21:03:58 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,41 @@ function feu_O () {
     strip.clear()
     strip.show()
 }
+function countdown () {
+    if (t > 0) {
+        basic.showNumber(t)
+        t = t - 1
+        basic.pause(1000)
+    } else if (t == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        strip.clear()
+        strip.show()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        Etat = 0
+    } else {
+    	
+    }
+}
+function feux_c2 () {
+    if (Etat == 1) {
+        countdown()
+    } else {
+        basic.pause(5000)
+        strip.clear()
+        strip.show()
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        Etat = 1
+        t = 10
+    }
+}
 function feux_c1 () {
     feu_V()
     feu_O()
@@ -24,11 +59,16 @@ function feu_V () {
     strip.clear()
     strip.show()
 }
+let Etat = 0
+let t = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.setBrightness(32)
 strip.clear()
 strip.show()
+t = 10
+Etat = 1
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
-    feux_c1()
+	
 })
