ac4d76a41c48b3f612ee5cfb00d70abf44685e84
mathyspecault <117740980+mathyspecault@users.noreply.github.com>
2022-12-02 09:59:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+enum RadioMessage {
+    message1 = 49434
+}
+function feu_V2 () {
+    strip.clear()
+    strip.show()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+}
 function feu_O () {
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
     strip.show()
@@ -5,11 +14,19 @@ function feu_O () {
     strip.clear()
     strip.show()
 }
+function feu_R2 () {
+    strip.clear()
+    strip.show()
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+}
 function countdown () {
     if (t > 0) {
         basic.showNumber(t)
         t = t - 1
         basic.pause(1000)
+    } else if (t == 5) {
+        radio.sendString("Fo")
     } else if (t == 0) {
         basic.showLeds(`
             . . . . .
@@ -27,6 +44,9 @@ function countdown () {
     	
     }
 }
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Sy")
+})
 function feux_c2 () {
     if (Etat == 1) {
         countdown()
@@ -45,6 +65,11 @@ function feux_c1 () {
     feu_O()
     feu_R()
 }
+function feu_O2 () {
+    strip.clear()
+    strip.show()
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+}
 function feu_R () {
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     strip.show()
@@ -52,6 +77,19 @@ function feu_R () {
     strip.clear()
     strip.show()
 }
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Fo") {
+        feu_O2()
+        basic.pause(2000)
+        feu_R2()
+        t = 20
+        Etat = 1
+    }
+    if (receivedString == "Sy") {
+        t = 7
+        Etat = 1
+    }
+})
 function feu_V () {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     strip.show()
@@ -59,16 +97,25 @@ function feu_V () {
     strip.clear()
     strip.show()
 }
-let Etat = 0
 let t = 0
+let Etat = 0
 let strip: neopixel.Strip = null
+radio.setGroup(10)
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.setBrightness(32)
 strip.clear()
 strip.show()
-t = 10
-Etat = 1
+Etat = 2
 strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . # . .
+    `)
 basic.forever(function () {
-	
+    if (Etat == 1) {
+        countdown()
+    }
 })
